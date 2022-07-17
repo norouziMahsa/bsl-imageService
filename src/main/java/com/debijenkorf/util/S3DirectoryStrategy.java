c8650689b48e31fffd8e7bb9ab8466ee7ce80be0
norouziMahsa <67599259+norouziMahsa@users.noreply.github.com>
2022-07-17 19:33:20 +0200

    fix s3 directory strategy when file name length is greater than 4 but smaller than or equal to 8

--- src/main/java/com/debijenkorf/util/S3DirectoryStrategy.java
+++ src/main/java/com/debijenkorf/util/S3DirectoryStrategy.java
@@ -5,9 +5,14 @@ import org.springframework.util.StringUtils;
 
 /**
  * Defines the s3 directory strategy
- * if the length of the image name (without extension) is smaller or equal to 4 characters,
- * or greater than 4 but smaller than or equal to 8, the image is stored directly,
- * otherwise,
+ *
+ * if the length of the image name (without extension) is smaller than or equal to 4,
+ * the image is stored directly to the initial directory,
+ *
+ * if the length of the image name (without extension) is greater than 4 but smaller than or equal to 8,
+ * the first 4 chars is considered as sub directory
+ *
+ * if the length of the image name (without extension) is greater than 8,
  * the first 4 chars is considered as initial sub directory, and the second 4 chars as second sub directory
  */
 public final class S3DirectoryStrategy {
@@ -18,14 +23,14 @@ public final class S3DirectoryStrategy {
         var originalImageNameWithoutExtension = FilenameUtils.removeExtension(reference);
         originalImageNameWithoutExtension = originalImageNameWithoutExtension.replaceAll("/", "_");
         var originalNameLength = originalImageNameWithoutExtension.length();
+        String[] parts = originalImageNameWithoutExtension.split("(?<=\\G.{4})");
         if (originalNameLength <= 4) {
             return predefineType + "/" + originalImageNameWithoutExtension;
         }
         else if (originalNameLength <= 8) {
-            return predefineType + "/" + originalImageNameWithoutExtension;
+            return predefineType + "/" + parts[0] + "/" + originalImageNameWithoutExtension;
         }
         else {
-            String[] parts = originalImageNameWithoutExtension.split("(?<=\\G.{4})");
             return predefineType + "/" + parts[0] + "/" + parts[1] + "/" + originalImageNameWithoutExtension;
         }
     }
